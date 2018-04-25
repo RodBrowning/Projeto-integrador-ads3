19e0fc9859b735713f08d5a9be819026335c971e
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2018-04-25 19:16:51 -0300

    Terminando lay-out responsivo na pagina de configuração

--- public/pages/configuracao.php
+++ public/pages/configuracao.php
@@ -1,6 +1,6 @@
 <?php echo $_REQUEST['page']?>
 <div class="row padding-2">
-	<div class="col-8 titleDiv marginAuto">
+	<div class="col-lg-8 col-md-9 titleDiv marginAuto">
 		<p>Configurações</span></p>
 		<hr>
 	</div>
@@ -18,13 +18,13 @@
 					<hr class="linhaPreta">					
 				</div>
 
-				<div class="up-date-values col-lg-11 marginAuto">
+				<div class="form-div col-lg-11 col-md-10 marginAuto">
 					
 					<div class="div-update display-block ">
 						<p class="display-inline-block ">Primeira Hora: <input type="number" step="any" id="primeira-hora"></p>
 						<p class="display-inline-block">Demais Horas: <input type="number" id="demais-hora"></p>
-						<div class="display-inline-block">
-							<button class="my-btn my-btn-success">Salvar</button>
+						<div class="button-ajuste-cobranca display-inline-block">
+							<button class=" my-btn my-btn-success">Salvar</button>
 						</div>
 					</div>
 
@@ -52,12 +52,50 @@
 		
 	</div>
 
-	<div class="col-8 titleDiv marginAuto">
-		<p>Cadastro de funcionários</span></p>
-		<hr>		
-	</div>
-
-	<div class="col-8 titleDiv marginAuto">
+		<div class="col-lg-8 col-md-9 titleDiv marginAuto mt-4">
+			<p>Cadastro de funcionários</span></p>
+			<hr>
+		</div>	
+		<div class="col-10 marginAuto mb-4">
+			<fieldset class="col-md-12">
+				<legend ">Cadastrar</legend>
+				<div class="form-cad-func col-lg-10 text-center marginAuto">					
+					<div class="display-block">
+						<label class="width-label-1">Nome:</label><input class="input-name-func" type="text" id="primeira-hora">
+					</div>
+				</div>
+				<div class="form-cad-func col-lg-10 marginAuto">					
+					<div class="display-block">
+						<label class="display-inline-block width-label-1">CFP:</label><input class="width-input-2" type="text" id="primeira-hora">
+					
+						<label class="display-inline-block width-label-2">Tipo acesso:</label>
+							<select class="width-select">
+								<option value="" selected disabled hidden>Select</option>
+								<option value="volvo">Volvo</option>
+							  	<option value="saab">Saab</option>
+								<option value="mercedes">Mercedes</option>
+							  	<option value="audi">Audi</option>
+							</select>
+					</div>
+				</div>
+				<div class="form-cad-func col-lg-10 marginAuto">					
+					<div class="display-block">
+						<label class="width-label-1">E-Mail:</label><input class="width-input-2" type="text">
+						<label class="width-label-2">Senha:</label><input class="width-input-1" type="text">
+					</div>
+				</div>
+				<div class="form-cad-func col-lg-10 marginAuto">					
+					<div class="display-block">
+						<div class="button-cadastro-func display-inline-block">
+							<button class="my-btn my-btn-success">Salvar</button>
+						</div>
+					</div>
+				</div>
+			</fieldset>
+		</div>
+		</div>
+	
+	<div class="col-lg-8 col-md-9 titleDiv marginAuto mt-4">
 		<p>Funcionários ativos</span></p>
 		<hr>		
 	</div>
@@ -95,7 +133,7 @@
 	</div>
 </div>
 <div class="row padding-2">
-	<div class="col-8 titleDiv marginAuto">
+	<div class="col-lg-8 col-md-9 titleDiv marginAuto">
 		<p>Funcionários inativos</p>
 		<hr>		
 	</div>
